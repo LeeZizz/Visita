317a88404ecb4d289c7481f32a14b890fb84fcc6
danielthanh <danieldoisme@gmail.com>
2025-12-05 11:28:07 +0700

    Add AdminRepository and update ApplicationInitConfig for admin user creation

--- src/main/java/com/visita/security/ApplicationInitConfig.java
+++ src/main/java/com/visita/security/ApplicationInitConfig.java
@@ -1,53 +1,37 @@
 package com.visita.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.visita.entities.Gender;
-import com.visita.entities.RoleEntity;
-import com.visita.entities.UserEntity;
-import com.visita.repositories.RoleRepository;
-import com.visita.repositories.UserRepository;
+import com.visita.entities.AdminEntity;
+import com.visita.repositories.AdminRepository;
 
 import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
-@RequiredArgsConstructor
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ApplicationInitConfig {
 
-	@Autowired
 	PasswordEncoder passwordEncoder;
 
-	@Autowired
-	private RoleRepository roleRepository;
+	public ApplicationInitConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Bean
-	ApplicationRunner applicationRunner(UserRepository userRepository) {
+	ApplicationRunner applicationRunner(AdminRepository adminRepository) {
 		return args -> {
-			if (userRepository.findByUserName("admin").isEmpty()) {
-//                RoleEntity roleAdmin = roleRepository.findByRoleName("ADMIN");
-//                roleAdmin.setRoleName("ADMIN");
-//                roleRepository.save(roleAdmin);
-				RoleEntity roleAdmin = roleRepository.findByRoleName("ADMIN").orElseGet(() -> {
-					RoleEntity newRole = new RoleEntity();
-					newRole.setRoleName("ADMIN");
-					return roleRepository.save(newRole);
-				});
-
-				UserEntity userEntity = UserEntity.builder().userName("admin")
-						.passWord(passwordEncoder.encode("admin123")) // password: admin123
-						.fullName("Administrator").email("admin@gmail.com").gender(Gender.valueOf("MALE"))
-						.phone("12345678910").role(roleAdmin).build();
-				userRepository.save(userEntity);
-				log.warn("Admin user created with username: admin and password: admin123");
+			if (adminRepository.findByUsername("admin").isEmpty()) {
+				AdminEntity adminEntity = AdminEntity.builder().username("admin")
+						.password(passwordEncoder.encode("admin")) // password: admin
+						.fullName("Administrator").email("admin@example.com").build();
+				adminRepository.save(adminEntity);
+				log.warn("Admin user created with username: admin and password: admin");
 			}
 		};
 	}
