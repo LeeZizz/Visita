78288c9e0ffa30a7623e8aac15639998a7273669
danielthanh <danieldoisme@gmail.com>
2025-12-05 17:30:06 +0700

    Add separate login flows for users and admins, enhancing authentication logic

--- src/main/java/com/visita/dto/request/AuthenticationRequest.java
+++ src/main/java/com/visita/dto/request/AuthenticationRequest.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class AuthenticationRequest {
-
-	private String email;
+    
+    private String email;    // Used for Customers
+    private String username; // Used for Admins
 	private String password;
 }
